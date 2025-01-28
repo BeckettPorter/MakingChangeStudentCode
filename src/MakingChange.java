fb371188b4f59cbd80d6125f3d43a96212539513
BeckettPorter <porterbeckett@gmail.com>
2025-01-28 10:35:02 -0800

    Deleted all my code again :)

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 /**
  * The class Making Change solves a classic problem:
@@ -10,88 +9,37 @@ import java.util.ArrayList;
 
 public class MakingChange
 {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
-
-
     public static long countWays(int target, int[] coins)
     {
-
-        // Sort the coins from smallest to largest.
-        int[] sortedCoins = sortSmallToLarge(coins);
-
         int totalWays = 0;
 
 
-
-        // Two things that can be modified, the target amount and the coins[] array.
-
-        // Need to somehow figure out how to make this applicable to more than just two coins though.
-
-
-
-
-
-        System.out.println(twoCoinComparison(sortedCoins[0], sortedCoins[1], target));
-
-        for (int smallCoinIndex = 0; smallCoinIndex < sortedCoins.length - 1; smallCoinIndex++)
+        for (int testCoin : coins)
         {
-            for (int largeCoinIndex = smallCoinIndex; largeCoinIndex < sortedCoins.length; largeCoinIndex++)
+            int testTarget = target;
+
+            for (int comparisonCoin : coins)
             {
-                totalWays += twoCoinComparison(sortedCoins[smallCoinIndex], sortedCoins[largeCoinIndex], target);
+                if (testCoin != comparisonCoin)
+                {
+                    while (testTarget > 0)
+                    {
+                        if (canMakeTarget(testTarget, testCoin))
+                        {
+                            totalWays++;
+                        }
+                        testTarget -= comparisonCoin;
+                    }
+                }
             }
+
         }
-
-
 
         return totalWays;
     }
 
-    // for small coin, get max num we can fit, then subtract large coin from total, repeat
-    private static int twoCoinComparison(int smallCoin, int largeCoin, int target)
+    private static boolean canMakeTarget(int target, int coin)
     {
-        int numCombosFound = 0;
-
-        // Given two coins, this should find the number of combos we can make with them.
-        while (target > smallCoin)
-        {
-            int currentTestAmount = smallCoin;
-
-            // Add as many small coins as we can.
-            while (currentTestAmount < target)
-            {
-                currentTestAmount += smallCoin;
-            }
-
-            if (target % currentTestAmount == 0)
-            {
-                numCombosFound++;
-            }
-
-            target -= largeCoin;
-        }
-
-        return numCombosFound;
-    }
-
-    private static int[] sortSmallToLarge(int[] ar)
-    {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        for (int num : ar)
-        {
-            arrayList.add(num);
-        }
-        arrayList.sort(null);
-
-        int[] sortedList = new int[ar.length];
-
-        for (int i = 0; i < arrayList.size(); i++)
-        {
-            sortedList[i] = arrayList.get(i);
-        }
-        return sortedList;
+        return (target % coin == 0);
     }
 }

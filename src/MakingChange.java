7491f046a8681d5784982d9c511c28512a5386ca
BeckettPorter <porterbeckett@gmail.com>
2025-02-03 14:24:08 -0800

    memoization is still pain but it works now

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -10,15 +10,15 @@ import java.util.ArrayList;
 
 public class MakingChange
 {
-
-    private static int[][] memoization;
+    // 1st [] is target, 2nd [] is coin index.
+    private static long[][] memoization;
 
     public static long countWays(int target, int[] coins)
     {
         int[] sortedCoins = sortSmallToLarge(coins);
 
         // X is up to target and y is the coins.
-        memoization = new int[target][coins.length];
+        memoization = new long[target + 1][coins.length];
 
         for (int i = 0; i < coins.length; i++)
         {
@@ -28,46 +28,55 @@ public class MakingChange
         return count(target, sortedCoins, 0);
     }
 
-    private static int count(int target, int[] coins, int index)
+    private static long count(int target, int[] coins, int index)
     {
+        // If target is 0, we have successfully reached a combo of coins that works.
         if (target == 0)
         {
             return 1;
         }
-        if (target < 0)
+
+        // If target is negative, or we have no more coins to use.
+        if (target < 0 || index == coins.length)
         {
             return 0;
         }
 
-        // If we have no more coins in the array.
-        if (index == coins.length)
+
+        // Include, we set target equal to target - the current coin. (including this coin)
+        long include = 0;
+        int checkTarget = target - coins[index];
+        // If we have already calculated this include, use that. Otherwise, calculate it.
+
+        if (checkTarget >= 0)
         {
-            return 0;
+            if (memoization[checkTarget][index] != 0)
+            {
+                include = memoization[checkTarget][index];
+            }
+            else
+            {
+                include = count(checkTarget, coins, index);
+                memoization[checkTarget][index] = include;
+            }
+        }
+        // Exclude, we keep target the same but go to the next coin (excluding this coin)
+        long exclude = 0;
+        int checkIndex = index + 1;
+
+        if (checkIndex < coins.length)
+        {
+            if (memoization[target][checkIndex] != 0)
+            {
+                exclude = memoization[target][checkIndex];
+            }
+            else
+            {
+                exclude = count(target, coins, checkIndex);
+                memoization[target][checkIndex] = exclude;
+            }
         }
 
-        int nInclude = 0;
-        if (target - coins[index] >= 0)
-        {
-            nInclude = memoization[target - coins[index]][index];
-        }
-
-        // Include
-        if (nInclude != 0)
-        {
-            return nInclude;
-        }
-        int include = count(target - coins[index], coins, index);
-
-        if (target - coins[index] >= 0)
-        {
-            memoization[target - coins[index]][index] = include;
-        }
-
-
-        // Exclude
-        int exclude = count(target, coins, index + 1);
-
-        memoization[target][index] = exclude;
 
         return include + exclude;
     }

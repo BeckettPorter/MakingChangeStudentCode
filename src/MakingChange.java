d0aa365dd417dc24aabcc9d61255e33a219a0dc0
BeckettPorter <porterbeckett@gmail.com>
2025-02-05 13:41:22 -0800

    Trying to branch

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -25,69 +25,6 @@ public class MakingChange
         return count(target, sortedCoins, 0);
     }
 
-    // Recursive method that counts the number of ways to make up target int with an int array of numbers (aka coins).
-    private static long count(int target, int[] coins, int index)
-    {
-        // If target is 0, we have successfully reached a combo of coins that works, so return 1.
-        if (target == 0)
-        {
-            return 1;
-        }
-
-        // If target is negative, or we have no more coins to use, return 0.
-        if (target < 0 || index == coins.length)
-        {
-            return 0;
-        }
-
-        // Include, we set target equal to target - the current coin. (i.e. including this coin)
-        long include = 0;
-        int checkTarget = target - coins[index];
-
-        // If our target we are checking is not a negative number.
-        if (checkTarget >= 0)
-        {
-            // Then check if we have already calculated a value at this point in the array.
-            if (memoization[checkTarget][index] != 0)
-            {
-                // If we have, use that value.
-                include = memoization[checkTarget][index];
-            }
-            // If not, then calculate it for the first time.
-            else
-            {
-                include = count(checkTarget, coins, index);
-                // Then set that point in the array to that new calculated value.
-                memoization[checkTarget][index] = include;
-            }
-        }
-
-        // Exclude, we keep target the same but go to the next coin (i.e. excluding this coin)
-        long exclude = 0;
-        int checkIndex = index + 1;
-
-        // If our check index is a valid index
-        if (checkIndex < coins.length)
-        {
-            // Then check if we have already calculated a value at this point in the array.
-            if (memoization[target][checkIndex] != 0)
-            {
-                // If we have, use that value.
-                exclude = memoization[target][checkIndex];
-            }
-            // If not, then calculate it for the first time.
-            else
-            {
-                exclude = count(target, coins, checkIndex);
-                // Then set that point in the array to that new calculated value.
-                memoization[target][checkIndex] = exclude;
-            }
-        }
-
-        // Add together the included and excluded values and return that.
-        return include + exclude;
-    }
-
     // Sort a given int array from smallest to largest.
     private static int[] sortSmallToLarge(int[] ar)
     {
